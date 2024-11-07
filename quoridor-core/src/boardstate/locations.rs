e04b078677c4f0e7baf0223e00895d85b042006b
Wolfram Friele <wolframfriele@gmail.com>
2024-11-07 21:46:32 +0100

    add method to convert from square number to coordinate

--- quoridor-core/src/boardstate/locations.rs
+++ quoridor-core/src/boardstate/locations.rs
@@ -1,9 +1,14 @@
+pub struct Coordinate {
+    pub x: u8,
+    pub y: u8,
+}
+
 #[derive(Clone, Hash)]
-pub struct Location {
+pub struct PawnLocation {
     square: u8,
 }
 
-impl Location {
+impl PawnLocation {
     /// Create a new Location, the number is the square on the board where counting starts in the
     /// bottom left corner and goes right. So coordinate A1 is square 1, B1 is square 2 and A2 is
     /// square 10 etc.
@@ -11,7 +16,7 @@ impl Location {
     /// The square number must be between 1 and 81, or else an error will be returned.
     pub fn build(square: u8) -> Result<Self, String> {
         if (1..=81).contains(&square) {
-            Ok(Location { square })
+            Ok(PawnLocation { square })
         } else {
             Err(format!(
                 "The square should be in range 1..=81, but was: {square}"
@@ -19,31 +24,41 @@ impl Location {
         }
     }
 
-    pub fn from_notation(coordinate_notation: &str) -> Result<Self, String> {
-        // Convert quoridor notation string to coordinate; for example C2 is 12
-
-        Ok(Location { square: 12 })
-    }
-
     pub fn get_square(&self) -> u8 {
         assert!((1..=81).contains(&self.square));
         self.square
     }
 
-    fn new_location_from_direction(&self, direction: Direction) -> Result<Location, String> {
+    pub fn get_coordinate(&self) -> Coordinate {
+        let remainder = &self.square % 9;
+        if remainder == 0 {
+            Coordinate {
+                x: 9,
+                y: &self.square / 9,
+            }
+        } else {
+            Coordinate {
+                x: remainder,
+                y: (&self.square / 9) + 1,
+            }
+        }
+    }
+
+    fn new_location_from_direction(&self, direction: Direction) -> Result<PawnLocation, String> {
+        println!("{}", 21 % 9);
         match direction {
-            Direction::North => Location::build(&self.square + 9),
+            Direction::North => PawnLocation::build(&self.square + 9),
             Direction::East => {
-                if &self.square % 9 != 0 {
-                    Location::build(&self.square + 1)
+                if self.get_coordinate().x != 9 {
+                    PawnLocation::build(&self.square + 1)
                 } else {
                     Err(format!("Going East from square {0} is impossible since it is on the edge of the board", &self.square))
                 }
             }
-            Direction::South => Location::build(&self.square - 9),
+            Direction::South => PawnLocation::build(&self.square - 9),
             Direction::West => {
-                if ![1, 10, 19, 28, 37, 46, 55, 64, 73].contains(&self.square) {
-                    Location::build(&self.square - 1)
+                if self.get_coordinate().x != 1 {
+                    PawnLocation::build(&self.square - 1)
                 } else {
                     Err(format!("Going West from square {0} is impossible since it is on the edge of the board", &self.square))
                 }
@@ -52,7 +67,7 @@ impl Location {
     }
 }
 
-enum Direction {
+pub enum Direction {
     North,
     East,
     South,
@@ -96,16 +111,33 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_new_location_from_direction_east_succesfull() {
-        let starting_location = Location::build(5).unwrap();
+    fn coordinate_from_location() {
+        let combinations = [
+            (PawnLocation::build(1).unwrap(), Coordinate { x: 1, y: 1 }),
+            (PawnLocation::build(19).unwrap(), Coordinate { x: 1, y: 3 }),
+            (PawnLocation::build(21).unwrap(), Coordinate { x: 3, y: 3 }),
+            (PawnLocation::build(36).unwrap(), Coordinate { x: 9, y: 4 }),
+            (PawnLocation::build(45).unwrap(), Coordinate { x: 9, y: 5 }),
+            (PawnLocation::build(77).unwrap(), Coordinate { x: 5, y: 9 }),
+            (PawnLocation::build(81).unwrap(), Coordinate { x: 9, y: 9 }),
+        ];
+        for (input, expected) in combinations {
+            assert_eq!(input.get_coordinate().x, expected.x);
+            assert_eq!(input.get_coordinate().y, expected.y);
+        }
+    }
+
+    #[test]
+    fn new_pawn_location_from_direction_east_succesfull() {
+        let starting_location = PawnLocation::build(5).unwrap();
         let result = starting_location.new_location_from_direction(Direction::East);
         assert_eq!(result.unwrap().get_square(), 6)
     }
 
     #[test]
     #[should_panic]
-    fn test_new_location_from_direction_north_failed() {
-        let starting_location = Location::build(77).unwrap();
+    fn new_pawn_location_from_direction_north_failed() {
+        let starting_location = PawnLocation::build(77).unwrap();
         let result = starting_location
             .new_location_from_direction(Direction::North)
             .unwrap();
@@ -113,8 +145,8 @@ mod tests {
 
     #[test]
     #[should_panic]
-    fn test_new_location_from_direction_east_failed() {
-        let starting_location = Location::build(9).unwrap();
+    fn new_pawn_location_from_direction_east_failed() {
+        let starting_location = PawnLocation::build(9).unwrap();
         let result = starting_location
             .new_location_from_direction(Direction::East)
             .unwrap();
@@ -122,8 +154,8 @@ mod tests {
 
     #[test]
     #[should_panic]
-    fn test_new_location_from_direction_south_failed() {
-        let starting_location = Location::build(5).unwrap();
+    fn new_pawn_location_from_direction_south_failed() {
+        let starting_location = PawnLocation::build(5).unwrap();
         let result = starting_location
             .new_location_from_direction(Direction::South)
             .unwrap();
@@ -131,15 +163,15 @@ mod tests {
 
     #[test]
     #[should_panic]
-    fn test_new_location_from_direction_west_failed() {
-        let starting_location = Location::build(46).unwrap();
+    fn new_pawn_location_from_direction_west_failed() {
+        let starting_location = PawnLocation::build(46).unwrap();
         let result = starting_location
             .new_location_from_direction(Direction::West)
             .unwrap();
     }
 
     #[test]
-    fn test_new_walllocation_successfull() {
+    fn new_walllocation_successfull() {
         let invalid_square: Vec<u8> = vec![9, 18, 27, 36, 45, 54, 63];
         for square in 1..=71 {
             if !invalid_square.contains(&square) {
@@ -151,7 +183,7 @@ mod tests {
 
     #[test]
     #[should_panic]
-    fn test_new_walllocation_failed() {
+    fn new_walllocation_failed() {
         let location = WallLocation::build(36, WallOrientation::Horizontal).unwrap();
     }
 }
